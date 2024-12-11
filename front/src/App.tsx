8b8392f2ff4f617778aff2226b65854f52218706
pw_mbc <mbc@phiworks.co.kr>
2024-12-11 11:07:36 +0900

    grafana-volume명 변경, datasource provisioning 시 변수 이스케이프 및 uid 매칭 재조정, README 수정

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -115,11 +115,11 @@ const App: React.FC = () => {
                 }}
             />
             <iframe
-                src="http://localhost:3000/grafana/d-solo/ae6h49yp1qqyoa/new-dashboard?orgId=1&refresh=10s&panelId=1"
+                src="http://localhost:3000/grafana/d-solo/ae6h49yp1qqyoa/new-dashboard?orgId=1&refresh=10s&panelId=1&from=now-1h&to=now"
                 width="700" height="200"
             />
             <iframe
-                src="http://localhost:3000/grafana/d-solo/rYdddlPWk/node-exporter-full?orgId=1&refresh=10s&panelId=20"
+                src="http://localhost:3000/grafana/d-solo/rYdddlPWk/node-exporter-full?orgId=1&refresh=10s&panelId=20&from=now-24h&to=now"
                 width="450" height="200"></iframe>
 
         </div>
